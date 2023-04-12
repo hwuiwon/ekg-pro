d1a1f173b7b65854a3ae05f680ba9c4dbf9505ac
Alik-da-Geek <aliksemelianov@gmail.com>
2023-04-11 21:12:50 -0400

    Restyling chart component

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -1,5 +1,37 @@
 import React, { useState } from 'react'
 
+const Chart = () => {
+  const map = new Map()
+  map.set('First Chart', true)
+  map.set('Second Chart', false)
+  map.set('Third Chart', true)
+  const [selectedLayers, setLayers] = useState<Map<string, boolean>>(map)
+
+  const handleLayerSelect = (id: string) => {
+    // console.log(selectedLayers, id)
+    const newLayers = selectedLayers
+    newLayers?.set(id, !newLayers.get(id))
+    setLayers(newLayers)
+  }
+
+  return (
+    <div className="AnalysisContianer">
+      <div className="LayerContainer">
+        <p className="Subheading" style={{ margin: '10px' }}>
+          Layers
+        </p>
+        <LayersList
+          selectedLayers={selectedLayers}
+          onSelect={handleLayerSelect}
+        />
+      </div>
+      <div className="ChartContianer">
+        <ChartList selectedLayers={selectedLayers} />
+      </div>
+    </div>
+  )
+}
+
 interface LayersListProps {
   selectedLayers: Map<string, boolean>
   onSelect: (id: string) => void
@@ -9,21 +41,19 @@ interface ChartListProps {
   selectedLayers: Map<string, boolean>
 }
 
-const LayersList = ({
-  selectedLayers: selectedLayers,
-  onSelect,
-}: LayersListProps) => {
+const LayersList = (props: LayersListProps) => {
   const layerComponentList: Array<JSX.Element> = []
-  selectedLayers.forEach((selected: boolean, id: string) => {
+  console.log(props.selectedLayers)
+  props.selectedLayers.forEach((selected: boolean, id: string) => {
     layerComponentList.push(
-      <div
-        key={id}
-        style={{
-          background: selected ? 'green' : 'gray',
-        }}
-        onClick={() => onSelect(id)}
-      >
-        {selected}
+      <div className="Layer" key={id} onClick={() => props.onSelect(id)}>
+        <p className="LayerText">{id}</p>
+        <div
+          className="LayerButton"
+          style={{
+            background: selected ? 'green' : 'gray',
+          }}
+        />
       </div>
     )
   })
@@ -36,15 +66,7 @@ const ChartList = ({ selectedLayers }: ChartListProps) => {
   selectedLayers.forEach((selected: boolean, id: string) => {
     if (selected) {
       chartComponentList.push(
-        <div
-          key={id}
-          style={{
-            width: '100%',
-            height: '50px',
-            backgroundColor: 'white',
-            marginBottom: '10px',
-          }}
-        >
+        <div key={id} className="ChartContainer">
           {id}
         </div>
       )
@@ -54,42 +76,6 @@ const ChartList = ({ selectedLayers }: ChartListProps) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       {chartComponentList}
-    </div>
-  )
-}
-
-const Chart = () => {
-  const map = new Map()
-  map.set('First Chart', true)
-  map.set('Second Chart', true)
-  map.set('Third Chart', true)
-  const [selectedLayers, setLayers] = useState<Map<string, boolean>>(map)
-
-  const handleLayerSelect = (id: string) => {
-    const newLayers = selectedLayers
-    newLayers?.set(id, !newLayers.get(id))
-    setLayers(newLayers)
-  }
-
-  // useEffect(() => {
-  //   const map = new Map()
-  //   map.set('First Chart', true)
-  //   map.set('Second Chart', true)
-  //   map.set('Third Chart', true)
-  //   setLayers(map)
-  // }, [])
-
-  return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ marginRight: '10px' }}>
-        <LayersList
-          selectedLayers={selectedLayers}
-          onSelect={handleLayerSelect}
-        />
-      </div>
-      <div>
-        <ChartList selectedLayers={selectedLayers} />
-      </div>
     </div>
   )
 }
